ab949abb5bde6bd31c2279493696d9adac71f77e
kindo-tk <tufan.kundu11@gmail.com>
2023-11-06 20:46:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+#weather app
+
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("index.html")
+
+@app.route("/weatherapp", methods = ['POST', 'GET'])
+def get_weatherdata():
+    url = 'https://api.openweathermap.org/data/2.5/weather'
+    params = {'q': request.form.get("city"),
+          'appid': request.form.get("appid"),
+          'units': request.form.get("units")
+        }
+    response = requests.get(url,params=params)
+    data = response.json()
+    return f"data : {data}"
+
+if __name__ == '__main__':
+    app.run(host = '0.0.0.0')
+    
