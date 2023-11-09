429ecbf55f6628e38e4875068d9bfb32e2933a82
kindo-tk <tufan.kundu11@gmail.com>
2023-11-09 15:15:50 +0530

    Some updates, api key enabled by default

--- app.py
+++ app.py
@@ -8,18 +8,14 @@ app = Flask(__name__)
 @app.route('/')
 def homepage():
     return render_template("index.html")
-
 @app.route("/weatherapp", methods = ['POST', 'GET'])
 def get_weatherdata():
-    url = 'https://api.openweathermap.org/data/2.5/weather'
-    params = {'q': request.form.get("city"),
-          'appid': request.form.get("appid"),
-          'units': request.form.get("units")
-        }
-    response = requests.get(url,params=params)
+    city = request.form.get("city")
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=2e18e4ef351fba6f290c44d99284aa3c&units=metric"
+    response = requests.get(url)
     data = response.json()
-    city = data['name']
-    return f"data : {data}, city:{city}"
+    temp = data['main']['temp']
+    return f"city : {city},\n temperature:{temp}"
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
