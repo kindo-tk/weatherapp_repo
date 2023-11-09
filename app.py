d457b25ade8b6da3b422a11b4beb627836d8df11
kindo-tk <tufan.kundu11@gmail.com>
2023-11-09 15:23:58 +0530

    minor update of exception handling

--- app.py
+++ app.py
@@ -10,12 +10,15 @@ def homepage():
     return render_template("index.html")
 @app.route("/weatherapp", methods = ['POST', 'GET'])
 def get_weatherdata():
-    city = request.form.get("city")
-    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=2e18e4ef351fba6f290c44d99284aa3c&units=metric"
-    response = requests.get(url)
-    data = response.json()
-    temp = data['main']['temp']
-    return f"city : {city},\n temperature:{temp}"
+    try:
+        city = request.form.get("city")
+        url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=2e18e4ef351fba6f290c44d99284aa3c&units=metric"
+        response = requests.get(url)
+        data = response.json()
+        temp = data['main']['temp']
+        return f"city : {city},\n temperature:{temp}"
+    except:
+        return f"City not found! try some other city"
 
 if __name__ == '__main__':
     app.run(host = '0.0.0.0')
